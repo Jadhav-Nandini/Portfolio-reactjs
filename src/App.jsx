cb7e88946360c1e9b3b96f8aa43b66ba171dae3e
Jadhav-Nandini <nandinij780@gmail.com>
2025-04-13 03:05:23 +0530

    create hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import About from './components/About'
 import { ThemeProvider } from './context/themeContext.jsx'
-import Projects from './components/Projects.jsx'
-import Skills from './components/Skills.jsx'
 
 const App = () => {
 
@@ -21,21 +18,3 @@ export default App
 
 
 
-// import React, { useEffect, useState } from 'react';
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import About from './components/About';
-// import { ThemeProvider } from './context/themeContext.jsx';
-
-// const App = () => {
-
-//   return (
-//     <ThemeProvider value={{ darkMode, toggleDarkMode }}>
-//       <Navbar />
-//       <Hero />
-//       <About />
-//     </ThemeProvider>
-//   );
-// };
-
-// export default App;
