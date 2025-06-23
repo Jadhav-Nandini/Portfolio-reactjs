e1888198ead4b4c2794b5d2c697d78dead72c32a
Jadhav-Nandini <nandinij780@gmail.com>
2025-06-24 02:52:17 +0530

    add image path

--- src/data/projectsDetail.jsx
+++ src/data/projectsDetail.jsx
@@ -3,7 +3,7 @@ export const projects = [
 {
     title: "Mern Auth System",
     description:"Secure your acess",
-    image:"public/auth.png",
+    image:"/auth.png",
     github:"https://github.com/Jadhav-Nandini/Mern-Auth-System",
     live: "https://mern-auth-system-01.vercel.app/" ,
     tech:["MONGODB", "EXPRESS", "REACTJS", "NODEJS", "JSON WEB TOKEN", "TAILWINDCSS"],
@@ -14,7 +14,7 @@ export const projects = [
 {
     title: "Weather App",
     description:"Check the wheather",
-    image:"public/weather.png",
+    image:"/weather.png",
     github:"https://github.com/Jadhav-Nandini/Weather-App",
     live:"https://weather-now-checker.netlify.app/" ,
     tech:["HTML", "CSS", "JS", "API"],
@@ -24,7 +24,7 @@ export const projects = [
 {
     title: "Clone",
     description:"clone of sidcup family golf ",
-    image:"public/golf.png",
+    image:"/golf.png",
     github:"https://github.com/Jadhav-Nandini/Family-Golf-Website-Clone",
     tech:["HTML", "CSS", "JS", "GSAP"],
     
